0c6dcb85d5ab26c6ba35ad31f0115f34cd4ff596
Tendrun <mikisz2002@gmail.com>
2024-03-23 16:19:05 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        long startTime = System.nanoTime();
+
         int[] arr = new int[]
                 {
                 7,5,3,4, //T1
@@ -9,8 +12,15 @@ public class Main {
                 1,6,3, //T3
                 5,4,2  //T4
                 };
-        IntSorter firstask = new IntSorter(arr, 4);
+        IntSorter firstask = new IntSorter(arr, 14);
         firstask.StartSorting();
 
+
+        long endTime = System.nanoTime();
+        long durationNano = (endTime - startTime);
+
+        double durationSec = (double)durationNano / 1_000_000_000;
+
+        System.out.println("Complete time: " + durationSec + " seconds");
     }
 }
